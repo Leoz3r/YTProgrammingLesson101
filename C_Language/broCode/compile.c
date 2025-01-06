e04c97f24269d545b324a75a07f651ddcffdc7af
Hzmth <ay4namirei@gmail.com>
2025-01-06 21:13:18 +0800

    compile in CMD
    compile and run a C program with cmd

---
+++ C_Language/broCode/compile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  
+  printf("I love pizza with balls!\n ");
+  printf("It's really f good! \n");
+
+  return 0;
+
+  /*
+  shortcut 
+  1. Windows key + R
+  2. type cmd 
+  3. OK 
+  
+  Check compiler availability
+  1. gcc --version 
+  2. cd "folder path/location" 
+
+  To compile 
+  1. gcc "Name of the file" 
+  2. a.exe
+  */
+
+}
