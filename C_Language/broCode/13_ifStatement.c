9336671c1e266aa630f80e9b5156d0701aa85694
Hzmth <ay4namirei@gmail.com>
2025-01-07 13:05:57 +0800

    Bro Code

--- C_Language/broCode/13_ifStatement.c
+++ C_Language/broCode/13_ifStatement.c
@@ -2,7 +2,23 @@
 
 int main(void) {
 
+  int age;
 
+  printf("\nEnter your age: ");
+  scanf("%d", &age);
+
+  if(age >= 18) {
+    printf("You are now signed up!\n");
+  }
+  else if (age == 0){
+    printf("You you can't signed up! You were just born!\n");
+  }
+  else if(age < 0) {
+    printf("You haven't been born yet!\n");
+  }
+  else {
+    printf("You are not old enough to sign up!\n"); 
+  }
 
   return 0;
 }
