742c0941b00a025a7f49d9a49dc63e9b792767dc
Hzmth <ay4namirei@gmail.com>
2025-01-12 23:35:07 +0800

    update

---
+++ C_Language/broCode/20_functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void birthday()
+{
+  printf("\nHappy birthday to you!");
+  printf("\nHappy birthday to you!");
+  printf("\nHappy birthday dear...YOU!");
+  printf("\nHappy birthday to you!\n");
+}
+
+int main(void) 
+{
+  birthday();
+  birthday();
+  birthday();
+
+  // if you need to use a code more than once, it's better practice to use functions 
+
+  return 0;
+}
