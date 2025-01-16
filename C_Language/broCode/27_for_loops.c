bdba47a3b86cfab36ab237b65f2df7bab8ed7d64
Hzmth <ay4namirei@gmail.com>
2025-01-16 23:55:48 +0800

    BroCode

---
+++ C_Language/broCode/27_for_loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  // for loop = repeats a section of code a limtied amount of times
+
+  for(int i =1; i <= 10; i-=3)
+  {
+    printf("%d\n", i);
+  }
+
+  return 0;
+}
