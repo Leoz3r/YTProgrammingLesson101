23b3980e12884b2e1895cd9d2292d6ff42595d15
Hzmth <ay4namirei@gmail.com>
2025-01-29 18:37:01 +0800

    BroCode

--- C_Language/broCode/45_bitwise_operators.c
+++ C_Language/broCode/45_bitwise_operators.c
@@ -11,7 +11,24 @@ int main(void)
   // << left shift
   // >> right shift
 
-  
+  int x = 6;  // 6 = 00000110
+  int y = 12; //12 = 00001100
+  int z = 0;  // 4 = 00000000
+
+  z =  x & y;
+  printf("AND = %d\n", z); // 4 = 00000100
+
+  z = x | y;
+  printf("OR = %d\n", z); // 14 = 00001110
+
+  z = x ^ y;
+  printf("XOR = %d\n", z); // 10 = 00001010
+
+  z = x << 2;
+  printf("SHIFT LEFT = %d\n", z); // 24 = 00011000
+
+  z = x >> 2;
+  printf("SHIFT RIGHT = %d\n", z); // 1 = 00000001
 
   return 0;
 }
