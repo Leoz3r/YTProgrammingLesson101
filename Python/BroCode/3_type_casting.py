ea824703d6925c91b7effa9e951f61fd9aee1773
Hzmth <ay4namirei@gmail.com>
2025-02-02 20:38:46 +0800

    BroCode

---
+++ Python/BroCode/3_type_casting.py
@@ -0,0 +1,27 @@
+# Typecasting = the process of converting a variable from one data type to another 
+#               str(), int(), float(), bool()
+
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+# print(type(name))
+
+# gpa = int(gpa)  # convert gpa to int
+# print(gpa)
+
+# age = float(age)  # convert age to float
+# print(age)
+
+"""
+age = str(age)  # convert age to string
+print(type(age)) 
+
+age += "1"  # concatenate age with string
+
+print(age)  # 251
+"""
+
+name = bool(name)
+print(name)  # True if the string is not empty, False if the string is empty
