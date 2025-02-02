8f8b895044b283de0009349470324f30561d18d0
Hzmth <ay4namirei@gmail.com>
2025-02-02 20:30:28 +0800

    BroCode

---
+++ Python/BroCode/2_variable.py
@@ -0,0 +1,52 @@
+# Variable = A container for a value (string, interger, float, boolean)
+#            A variable behaves as if it was the value it contains 
+
+# Strings 
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+# print(f"Hello {first_name}") 
+# print(f"You like {food}")
+# print(f"Your email is {email}")
+
+# Integers 
+age = 25
+quantity = 3
+num_of_students = 30 
+
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+# print(f"Your class has {num_of_students} students")
+
+# FLoat 
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# print(f"The price is ${price}")
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance}km")
+
+# Boolean 
+
+is_students = False
+for_sale = False
+is_online = True 
+
+# print(f"Are you a student: {is_students}")
+
+# if is_students:
+  # print("You are a student")
+# else: 
+  # print("You are NOT a student")
+
+# if for_sale:
+  # print("That item is for sale")
+# else:
+  # print("That item is NOT available")
+
+if is_online:
+  print("You are online")
+else:
+  print("You are offline")
