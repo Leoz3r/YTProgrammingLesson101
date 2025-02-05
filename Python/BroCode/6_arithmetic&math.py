fd3f139501248aeb7cf527a0ed22bef968b4d4e8
Hzmth <ay4namirei@gmail.com>
2025-02-05 15:13:01 +0800

    BroCode

---
+++ Python/BroCode/6_arithmetic&math.py
@@ -0,0 +1,45 @@
+import math 
+
+"""
+# friends = 10 
+
+# friends = friends + 1 
+# friends += 1  # Augmented assignment operator
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends =* 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+# remainder = friends % 3
+
+# print(friends)
+"""
+
+"""
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+result = max(x, y, z)
+result = min(x, y, z)
+
+print(result)
+"""
+
+"""
+x = 9.1
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x) # Round up even if it is 9.1
+# result = math.floor(x) # Round down
+
+print(result)
+"""
