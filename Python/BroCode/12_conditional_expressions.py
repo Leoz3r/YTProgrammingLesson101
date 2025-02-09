3967ab618ab852ac2741a89d82210a687e06511b
Leoz3r <justinhzmth@gmail.com>
2025-02-09 18:34:19 +0800

    BroCode

---
+++ Python/BroCode/12_conditional_expressions.py
@@ -0,0 +1,20 @@
+# conditional expression = A one-line shortuct for the if-else statement (ternary operators)
+#                          Print or assign one of two values based on a condition
+#                          X if condition else Y
+
+num = 5
+a = 6
+b = 7
+age = 13
+temperature = 30 
+user_role = "guess"
+
+# print("Positive" if num > 0 else "Negataive")
+# result = "Even" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temperature > 20 else "COLD"
+acess_level = "Full Acess" if user_role == "admin" else "Limited Access"
+
+print(acess_level)
