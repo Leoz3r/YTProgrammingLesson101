2041f87fca0bb0a39398e450f8527065f7ea65ca
Hzmth <ay4namirei@gmail.com>
2025-02-13 19:25:22 +0800

    BroCode
    14. string indexing

---
+++ Python/BroCode/14_string_indexing.py
@@ -0,0 +1,21 @@
+# indexing = accessing alements of a sqequence using [] (indexing operators)
+#            [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-5])
+# print(credit_number[::3])
+
+
+# practical example 
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+
+# reverse 
+credit_number = credit_number[::-1]
+print(credit_number)
