a21a79e73831ac85f496a995ca29271d24a7128c
Hzmth <ay4namirei@gmail.com>
2025-02-21 21:20:53 +0800

    BroCode
    # collection = single "variable" used to store multiple values
    #   List  = [] ordered and changeable. Duplicates OK
    #   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
    #   Tuple = () ordered and unchangeable. Duplicates OK. FASTER

---
+++ Python/BroCode/21_list_sets_tuples.py
@@ -0,0 +1,38 @@
+# collection = single "variable" used to store multiple values
+#   List  = [] ordered and changeable. Duplicates OK
+#   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits = ("apple", "orange", "banana", "coconut", "coconut")
+# dir(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# print(fruits.index("apple"))
+print(fruits.count("coconut"))
+
+print(fruits)
+# for fruit in fruits:
+#  print (fruit)
+
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# list methods 
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("pineapple"))
+
+
+# fruits = {"apple", "orange", "banana", "coconut", "coconut"}
+# set methods 
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
